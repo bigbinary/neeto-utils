b6271ca9e914399fb9a90301399a5e8d4fea2e41
Vipul Goel <vipulgoel04@gmail.com>
2021-09-30 13:17:10 +0530

    Base setup

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./example/src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.md$/i,
+        use: "raw-loader",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, "example")],
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, "lib")],
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./example/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
